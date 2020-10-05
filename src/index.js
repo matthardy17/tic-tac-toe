5dcfb4fcb665f94ed868c9b8a50976830ab1147a
matt hardy <matthewhardy17@gmail.com>
2020-10-05 20:47:37 +0100

    added an updating clock to page

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@ function Square(props) {
   );
 }
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  render() {
+    return <div> {this.state.date.toLocaleTimeString()}</div>
+  }
+  componentDidMount() {
+    const oneSecond = 1000;
+    setInterval(() => {
+      this.setState({date: new Date() });
+    }, oneSecond);
+  }
+}
+
 class Title extends React.Component {
   render () {
     return (
@@ -98,6 +114,7 @@ class Game extends React.Component {
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
+        <Clock />
       </div>
     );
   }
