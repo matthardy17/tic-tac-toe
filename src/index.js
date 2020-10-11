0545ff19014b84e83c26ccffb559c862f51baaa5
matt hardy <matthewhardy17@gmail.com>
2020-10-11 19:19:01 +0100

    added a task/note taking component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
+import NewTask from "./NewTask";
+import TasksList from "./TaskList";
 
 function Square(props) {
   return (
@@ -114,8 +116,9 @@ class Game extends React.Component {
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
-        <Clock />
-      </div>
+          <Clock />;
+          <AppFunction />;
+        </div>
     );
   }
 }
@@ -140,6 +143,38 @@ function calculateWinner(squares) {
   return null;
 }
 
+function AppFunction() {
+  const [newTask, setNewTask] = useState({});
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
+  };
+
+  const [allTasks, setAllTasts] = useState([]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!newTask.title) return;
+    setAllTasts((prev) => [newTask, ...prev]);
+    setNewTask({});
+  };
+  const handleDelete = (taskIdToRemove) => {
+    setAllTasts((prev) => prev.filter(
+      (task) => task.id !== taskIdToRemove
+    ));
+  };
+
+  return (
+    <main>
+      <h1>Tasks</h1>
+      <NewTask
+        newTask={newTask}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <TasksList allTasks={allTasks} handleDelete={handleDelete} />
+    </main>
+  );
+}
 // ========================================
 
 ReactDOM.render(
